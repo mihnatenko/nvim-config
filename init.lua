aede8fe12cc9bd676c7160231b8fd97d17f938cf
mihnatenko <stormnco@gmail.com>
2024-04-08 13:36:28 +0300

    Split lazy config into `lazy-bootstrap` and `lazy-plugins`

--- init.lua
+++ init.lua
@@ -49,10 +49,20 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+-- [[ Setting options ]]
 require 'options'
+
+-- [[ Basic Keymaps ]]
 require 'keymaps'
+
+-- [[ Basic Autocommands ]]
 require 'autocommands'
-require 'lazyyy'
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+require 'lazy-bootstrap'
+
+-- [[ Configure and install plugins ]]
+require 'lazy-plugins'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
