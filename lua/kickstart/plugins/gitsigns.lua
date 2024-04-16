fe314f5a53c24827d62276cf15e26bd19afcdbcc
mihnatenko <stormnco@gmail.com>
2024-04-17 02:08:22 +0300

    make gitsigns thicker & add few keymaps

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -7,13 +7,33 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
   'lewis6991/gitsigns.nvim',
   opts = {
     signs = {
-      add = { text = '▌' },
-      change = { text = '▌' },
+      add = { text = '▊' },
+      change = { text = '▊' },
       delete = { text = '_' },
       topdelete = { text = '‾' },
       changedelete = { text = '~' },
       untracked = { text = '┆' },
     },
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+      end
+
+      -- Navigation
+      map('n', ']h', gs.next_hunk, 'Next hunk')
+      map('n', '[h', gs.prev_hunk, 'Previous hunk')
+
+      -- Actions
+      -- TODO: maybe it would make sense to modify descriptions to make `which-key` prettieer?
+      map('n', '<leader>hp', gs.preview_hunk, 'Preview hunk')
+      map('n', '<leader>hr', gs.reset_hunk, 'Reset hunk')
+      map('n', '<leader>hb', gs.blame_line, 'Blame line')
+      map('n', '<leader>hB', function()
+        gs.blame_line { full = true }
+      end, 'Blame line with diff')
+    end,
   },
 }
 
