abdf0a7917f11460a0c07a023cf8285ddaa729d6
mihnatenko <stormnco@gmail.com>
2024-04-17 02:10:50 +0300

    Change diagnostics icons

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -192,6 +192,11 @@ return { -- LSP Configuration & Plugins
         end,
       },
     }
+    local signs = { Error = '', Warn = '', Hint = '', Info = '' }
+    for type, icon in pairs(signs) do
+      local hl = 'DiagnosticSign' .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+    end
   end,
 }
 
