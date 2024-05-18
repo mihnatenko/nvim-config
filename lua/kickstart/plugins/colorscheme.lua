0508db795a70154b25a9014bc41a8f43dd1e0a73
mihnatenko <stormnco@gmail.com>
2024-05-19 01:34:06 +0300

    Return back to tokyonight colorscheme (for now)

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -5,17 +5,17 @@ return {
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'folke/tokyonight.nvim',
-    event = 'VeryLazy',
-    -- priority = 1000, -- make sure to load this before all the other start plugins
-    -- init = function()
-    --   -- Load the colorscheme here.
-    --   -- Like many other themes, this one has different styles, and you could load
-    --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    --   vim.cmd.colorscheme 'tokyonight-night'
-    --
-    --   -- You can configure highlights by doing something like
-    --   vim.cmd.hi 'Comment gui=none'
-    -- end,
+    -- event = 'VeryLazy',
+    priority = 1000, -- make sure to load this before all the other start plugins
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
+
+      -- You can configure highlights by doing something like
+      vim.cmd.hi 'Comment gui=none'
+    end,
   },
   {
     'catppuccin/nvim',
@@ -32,14 +32,15 @@ return {
   },
   {
     'ribru17/bamboo.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('bamboo').setup {
-        -- optional configuration here
-      }
-      require('bamboo').load()
-    end,
+    event = 'VeryLazy',
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --   require('bamboo').setup {
+    --     -- optional configuration here
+    --   }
+    --   require('bamboo').load()
+    -- end,
   },
 }
 
